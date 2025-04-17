c92a65d0857421178589ca4b8280fb7d14dfd26f
yurilover1 <hprjl7@163.com>
2025-04-17 11:11:21 +0800

    Initial commit of modified lb-foraging

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name="lbforaging",
     version="3.0.0",
     description="Level Based Foraging Environment",
-    author="Filippos Christianos",
-    url="https://github.com/semitable/lb-foraging",
+    author="yurilover1",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
     classifiers=[
         "Intended Audience :: Developers",
